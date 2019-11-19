b6e1693b80ff600a1b10d8499a8bc7d9e2903bd5
andylokandy <andylokandy@hotmail.com>
2019-11-19 21:05:33 +0800

    temp
    Signed-off-by: andylokandy <andylokandy@hotmail.com>

--- src/components/heatmap/index.tsx
+++ src/components/heatmap/index.tsx
@@ -112,12 +112,23 @@ function heatmapChart(
     )
     .ticks(10)
 
+  const labelAxis = categoryAxisGroup([
+    dummyCategories,
+    dummyCategories,
+    dummyCategories,
+  ])
+
   const xAxisSvg = axis
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + (height - 20) + ")")
+
   const yAxisSvg = axis
     .append("g")
     .attr("transform", "translate(0, " + margin.top + ")")
+
+  const labelAxisSvg = axis
+    .append("g")
+    .attr("transform", "translate(20, " + margin.top + ")")
 
   const brush = d3
     .brush()
@@ -184,8 +195,9 @@ function heatmapChart(
     lastZoomTransform = transform
 
     xAxisSvg.call(xAxis.scale(transform.rescaleX(xScale)))
-    yAxisSvg.call(yAxis.scale(transform.rescaleY(yScale)))
+    // yAxisSvg.call(yAxis.scale(transform.rescaleY(yScale)))
     hideTicksWithoutLabel(axis)
+    labelAxisSvg.call(labelAxis)
 
     ctx.clearRect(0, 0, canvasWidth, canvasHeight)
 
@@ -236,7 +248,7 @@ function createLayer(values: number[][]) {
     const x = pixel % valueWidth
     const y = Math.floor(pixel / valueWidth)
     const color = colorScale(values[x][y])
-    const rgb = color.substring(1, color.length - 1).split("", 2)
+    // const rgb = color.substring(1, color.length - 1).split("", 2)
 
     imageData[i] = parseInt(color.substring(1, 3), 16) // R
     imageData[i + 1] = parseInt(color.substring(3, 5), 16) // G
@@ -247,4 +259,82 @@ function createLayer(values: number[][]) {
   ctx.putImageData(sourceImageData, 0, 0)
 
   return canvas
+}
+
+type Category<T> = {
+  name: string,
+  start: T,
+  end: T
+}
+
+type KeyCategory = Category<string>
+type TransformedCategory = Category<number>
+
+const categoryAxisWidth = 20
+const dummyCategories: TransformedCategory[] = [
+  { name: "test1", start: 0, end: 100 },
+  { name: "test2", start: 100, end: 150 },
+  { name: "test3", start: 270, end: 280 },
+  { name: "test4", start: 290, end: 380 },
+  { name: "test5", start: 400, end: 500 },
+]
+
+function categoryAxisGroup(groups, scale) {
+  let transformedGroups = 
+  const categoryAxisGroup = selection => {
+    const g = selection.selectAll("g").data(groups)
+
+    g.enter()
+      .append("g")
+      .each(function(categories, i) {
+        const group = d3
+          .select(this)
+          .attr("transform", `translate(${i * (categoryAxisWidth + 8)}, 0)`)
+
+        const rects = group.selectAll("rect").data(categories)
+        const texts = group.selectAll("text").data(categories)
+
+        rects
+          .enter()
+          .append("rect")
+          .attr("width", categoryAxisWidth)
+          .attr("height", category => category.end - category.start)
+          .attr("x", 0)
+          .attr("y", category => category.start)
+          .attr("stroke", "black")
+          // .attr('stroke-linejoin', 'round')
+          .attr("fill", "grey")
+
+        texts
+          .enter()
+          .append("text")
+          .attr("fill", "black")
+          .attr("writing-mode", "tb")
+          .attr(
+            "transform",
+            category => `translate(0, ${category.end - 8}) rotate(180)`
+          )
+          .attr("dominant-baseline", "hanging")
+          .text(category => category.name)
+          .call(hideTextOverflow)
+      })
+  }
+
+  categoryAxisGroup.rescale = function(scale) {
+
+  }
+
+  return categoryAxisGroup
+}
+
+function transformCategoryGroups(groups, scale) {
+
+}
+
+function hideTextOverflow(text) {
+  text.style("display", category => {
+    const textWidth = category.name.length * 9
+    const rectWidth = category.end - category.start
+    return rectWidth > textWidth ? "" : "none"
+  })
 }
